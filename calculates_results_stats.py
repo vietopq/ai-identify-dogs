27ec633686c4c5e7c1e5545029064a6659a843ae
Tran Quoc Viet <vietopq@gmail.com>
2024-08-21 01:15:46 +0700

    update improvement coding

--- calculates_results_stats.py
+++ calculates_results_stats.py
@@ -82,10 +82,10 @@ def calculates_results_stats(results_dic):
     results_stats_dic['n_correct_breed'] = 0       
     
     # process through the results dictionary
-    for key in results_dic:
+    for key, value in results_dic.items():
          
         # Labels Match Exactly
-        if results_dic[key][2] == 1:
+        if value[2] == 1:
             results_stats_dic['n_match'] += 1
 
         # TODO: 5a. REPLACE pass with CODE that counts how many pet images of
@@ -99,16 +99,16 @@ def calculates_results_stats(results_dic):
         #           representing the number of correctly classified dog breeds.
         #           
         # Pet Image Label is a Dog AND Labels match- counts Correct Breed
-        if results_dic[key][3] == 1 and results_dic[key][2] == 1 :
+        if value[3] == 1 and value[2] == 1 :
             results_stats_dic['n_correct_breed'] += 1
         
         # Pet Image Label is a Dog - counts number of dog images
-        if results_dic[key][3] == 1 :
+        if value[3] == 1 :
             results_stats_dic['n_dogs_img'] += 1
             
             # Classifier classifies image as Dog (& pet image is a dog)
             # counts number of correct dog classifications
-            if results_dic[key][4] == 1:
+            if value[4] == 1:
                 results_stats_dic['n_correct_dogs'] += 1
 
         # TODO: 5b. REPLACE pass with CODE that counts how many pet images 
@@ -128,7 +128,7 @@ def calculates_results_stats(results_dic):
         else:
             # Classifier classifies image as NOT a Dog(& pet image isn't a dog)
             # counts number of correct NOT dog clasifications.
-            if results_dic[key][4] == 0:
+            if value[4] == 0:
                 results_stats_dic['n_correct_notdogs'] += 1
     # Calculates run statistics (counts & percentages) below that are calculated
     # using the counters from above.
--- print_results.py
+++ print_results.py
@@ -78,7 +78,7 @@ def print_results(results_dic, results_stats_dic, model,
 
     # Prints summary statistics (percentages) on Model Run
     print(" ")
-    for key in results_stats_dic:
+    for key, value in results_stats_dic.items():
         # TODO: 6b. REPLACE pass with CODE that prints out all the percentages 
         #           in the results_stats_dic dictionary. Recall that all 
         #           percentages in results_stats_dic have 'keys' that start with 
@@ -86,14 +86,14 @@ def print_results(results_dic, results_stats_dic, model,
         #           statement that determines if the key starts with the letter
         #           'p' and then you want to use a print statement to print 
         #           both the key and the value. Remember the value is accessed 
-        #           by results_stats_dic[key]
+        #           by value
         #
         if key[0] == 'p':
             label_print = key.replace("pct_match", "Match Labels")
             label_print = label_print.replace("pct_correct_dogs", "Dogs Correct")
             label_print = label_print.replace("pct_correct_breed", "Breeds Correct")
             label_print = label_print.replace("pct_correct_notdogs", "Not-a-Dog Correct")
-            print("{:.1f}% {:20}".format(results_stats_dic[key], label_print.title()))
+            print("{:.1f}% {:20}".format(value, label_print.title()))
 
 
     # IF print_incorrect_dogs == True AND there were images incorrectly 
